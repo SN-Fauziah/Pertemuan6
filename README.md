c5c08879fed4c1b86e5dd60e295df262556a371f
SN Fauziah <snfauziah@outlook.com>
2020-11-01 21:49:18 +0700

    update 7

--- README.md
+++ README.md
@@ -14,3 +14,23 @@ Kelas   : TI.20.B1<br>
 Pertama saya akan menjelaskan Tugas Pertemuan ke 5 yaitu Membuat Program untuk menginputkan biodata yang terdiri dari Nama lengkap, Nickname, NPM, TTL, No Telp, dan Alamat :
 ![Tugas5](Foto/Latihan5.PNG)<br>
 Berikut <i>source codenya</i> atau klik link berikut ([Tugas 5 Python](p5_tugas.py))
+```python
+print("Please enter your Full name : ")
+fullname=input()
+print("Please enter your Nickname : ")
+nickname=input()
+print("Please insert your NPM : ")
+NPM=int(input())
+print("Please enter your Born place : ")
+bornplace=input()
+print("Please insert your Age : ")
+age=int(input())
+print("Please enter your Home address : ")
+address=input()
+print("Please enter your Phone number : ")
+phonenumber=int(input())
+
+
+print("\nAssalamualaikum Wr, Wb.")
+print(f"Let me introduce my self.My name is {fullname}, but you can call me {nickname}.My NPM's {NPM}.I was born in {bornplace} and i am {age} years old. I am very glad if you want to invite my house in {address}.So, don't forget to call me before with the number {phonenumber}.")
+print("\nThank you.")```
--- p5_tugas.py
+++ p5_tugas.py
@@ -1,27 +1,19 @@
-print("Please enter full name : ")
+print("Please enter your Full name : ")
 fullname=input()
-
-print("please insert your Nickname : ")
+print("Please enter your Nickname : ")
 nickname=input()
-
-print("please enter your NPM : ")
-NPM=input()
-
-print("please enter your Born place : ")
+print("Please insert your NPM : ")
+NPM=int(input())
+print("Please enter your Born place : ")
 bornplace=input()
-
-print("please insert your age : ")
-age=input()
-
-print("please enter your home address : ")
+print("Please insert your Age : ")
+age=int(input())
+print("Please enter your Home address : ")
 address=input()
-
-print("please enter your phone number : ")
-phonenumber=input()
+print("Please enter your Phone number : ")
+phonenumber=int(input())
 
 
 print("\nAssalamualaikum Wr, Wb.")
-print(f"\nLet me introduce my self. My name is {fullname}, but you can call me {nickname}. My NPM is {NPM}. I was Born in {bornplace} and i am {age} years old. I am very glad if you want to invite my house in {address}. So, don't forget to call me before with the number {phonenumber}.")
+print(f"Let me introduce my self.My name is {fullname}, but you can call me {nickname}.My NPM's {NPM}.I was born in {bornplace} and i am {age} years old. I am very glad if you want to invite my house in {address}.So, don't forget to call me before with the number {phonenumber}.")
 print("\nThank you.")
-print("\n")
-
